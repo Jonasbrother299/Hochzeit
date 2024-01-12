59cda997864b8833758689ab147e38da0b4660f0
Jonas Mayer <jonas.mayer1999@gmx.de>
2024-01-12 16:03:09 +0100

    jiasdj

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import React from "react";
 
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path='/*' element={<App />}></Route>
+        <Route path="/*" element={<App />}></Route>
       </Routes>
     </Router>
   </React.StrictMode>
