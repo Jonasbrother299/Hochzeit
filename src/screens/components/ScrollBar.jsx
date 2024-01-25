8313be3d5c3067820f9d8a99f18fc7dc959848a4
Jonas Mayer <jonas.mayer@student.reutlingen-university.de>
2024-01-25 11:38:04 +0100

    a

--- src/screens/components/ScrollBar.jsx
+++ src/screens/components/ScrollBar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import "../../styles/components/ScrollBar.css";
+import "../../styles/components/Scrollbar.css";
 
 export default function ScrollBar() {
   useEffect(() => {
