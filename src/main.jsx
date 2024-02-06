ad1b533bae4b3bedc3887f632da4ead66dc39c55
Jonas Mayer <jonas.mayer@student.reutlingen-university.de>
2024-02-06 15:18:17 +0100

    Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import React from "react";
-
+import { Analytics } from '@vercel/analytics/react';
+ 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+            <Analytics />
     <Router>
       <Routes>
         <Route path="/*" element={<App />}></Route>
